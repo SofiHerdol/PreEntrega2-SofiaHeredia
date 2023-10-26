d715a4c304926a73daa6b1b04544ac8ae8e7beac
Sofía Heredia <sofiaherdol@gmail.com>
2023-10-26 00:46:09 -0300

    Commit 2da PreEntrega

--- js/main.js
+++ js/main.js
@@ -36,10 +36,8 @@ function nodeCharacter(array, container) {
 function mostrarPersonajeAleatorio(data) {
     const indiceAleatorio = Math.floor(Math.random() * data.length);
 
-    // Obtener la tarjeta correspondiente al índice aleatorio
     const tarjetaAleatoria = document.querySelectorAll('.card')[indiceAleatorio];
     alert(`¡Bienvenido al buscador de Rick y Morty! Te mostraremos un personaje aleatorio, para que veas como serán los resultados de tu búsqueda:`)
-    // Obtener y mostrar la información adicional en un alert
     const nombre = tarjetaAleatoria.querySelector('.characterName').textContent;
     const additionalInfo = tarjetaAleatoria.querySelector('.description').textContent;
     alert(`Personaje aleatorio: ${nombre}\n${additionalInfo}`);
@@ -49,16 +47,9 @@ function mostrarPersonajeAleatorio(data) {
 mostrarPersonajeAleatorio(data);
 
 function mostrarPersonajeMasCercanoAZ() {
-    // Obtener el primer personaje del array (el más cercano a la Z)
     const personajeMasCercanoAZ = data[0];
-
-    // Encontrar el elemento HTML correspondiente al primer personaje
     const elementoPersonaje = document.querySelector('.card:first-of-type');
-
-    // Obtener la descripción del personaje desde el elemento HTML
     const descripcionPersonaje = elementoPersonaje.querySelector('.description').textContent;
-
-    // Mostrar la información del personaje en un alert
     alert(`Personaje más cercano a la Z: ${personajeMasCercanoAZ.name}\n${descripcionPersonaje}`);
 }
 
@@ -67,20 +58,14 @@ mostrarPersonajeMasCercanoAZ(data);
 
 function buscarPersonajePorNombre() {
     let nombreBuscado;
-
     do {
         nombreBuscado = prompt("Ingrese el nombre del personaje que desea buscar:");
-
         if (nombreBuscado !== null && nombreBuscado !== "") {
             const personajeEncontrado = data.find(personaje => personaje.name === nombreBuscado);
-
             if (personajeEncontrado) {
                 const descripcionCompuesta = `Status: ${personajeEncontrado.status}\nSpecie: ${personajeEncontrado.species}\nGender: ${personajeEncontrado.gender}\nOrigin: ${personajeEncontrado.origin.name}\nLocation: ${personajeEncontrado.location.name}`;
                 alert(`Información del personaje:\nNombre: ${personajeEncontrado.name}\nDescripción:\n${descripcionCompuesta}`);
-
-                // Pregunta al usuario si desea buscar otro personaje
                 const buscarOtro = confirm("¿Desea buscar otro personaje?");
-
                 if (!buscarOtro) {
                     alert("Si deseas volver a buscar un personaje, recarga la página.");
                     break;
